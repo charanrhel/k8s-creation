f9aa7339ca95792aa34ce51bc1e85930c4d627bb
charanrhel <charan.rhel@gmail.com>
2023-02-22 11:20:25 -0800

    Creating K8S using Terraform

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "eks" {
   version = "19.5.1"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.24"
+  cluster_version = "1.25"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
@@ -60,7 +60,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t4g.small"]
 
       min_size     = 1
       max_size     = 3
@@ -70,7 +70,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t4g.small"]
 
       min_size     = 1
       max_size     = 2
